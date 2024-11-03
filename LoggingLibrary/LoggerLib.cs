afa6b559d57dfac4dd65d1e623c2aa7d7cf5f501
Eisenbahno <150165370+Eisenbahno@users.noreply.github.com>
2024-11-03 22:24:17 +0100

    updated loggerlib

--- LoggingLibrary/LoggerLib.cs
+++ LoggingLibrary/LoggerLib.cs
@@ -8,4 +8,12 @@ namespace LoggingLibrary
         static LoggerLib()
         {
             var log4netConfig = new FileInfo("log4net.config");
-            XmlConfigur
+            XmlConfigurator.Configure(log4netConfig);
+        }
+
+        public static ILog GetLogger<T>()
+        {
+            return LogManager.GetLogger(typeof(T));
+        }
+    }
+}
