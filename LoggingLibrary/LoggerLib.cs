fc84b3fd789697768e67b042e8fc4c33d5ac4961
Eisenbahno <150165370+Eisenbahno@users.noreply.github.com>
2024-11-03 22:23:21 +0100

    added logging lib

---
+++ LoggingLibrary/LoggerLib.cs
@@ -0,0 +1,11 @@
+﻿using log4net;
+using log4net.Config;
+
+namespace LoggingLibrary
+{
+    public static class LoggerLib
+    {
+        static LoggerLib()
+        {
+            var log4netConfig = new FileInfo("log4net.config");
+            XmlConfigur
